b6ccf1c6fe148a9481ff2425e77817dde6577273
sldstu <hz202300241@wmsu.edu.ph>
2025-07-07 01:56:43 +0800

    fin

--- start.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd /d "c:\xampp\htdocs\AFK"
-echo Starting Minecraft AFK Calculator...
-echo.
-echo The application will be available at: http://localhost:5000
-echo Press Ctrl+C to stop the application
-echo.
-dotnet run --urls "http://localhost:5000"
-pause
